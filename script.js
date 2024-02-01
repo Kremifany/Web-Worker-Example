60f63d25b58adf5315497ffca38a0d9c289db085
Kremifany <34751960+Kremifany@users.noreply.github.com>
2024-01-31 23:32:49 -0500

    backdrop init

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
+
 const worker = new Worker("worker.js");
 //const sumButton = document.querySelector("#sumButton");
 const bgButton = document.querySelector("#bgButton");
-
+let state = "online";
 const result = document.querySelector(".result");
 
 worker.postMessage("this message was sent from main script to worker");
-
+function toggleBackdrop() {
+  const backdrop = document.getElementById("backdrop");
+  backdrop.classList.toggle("hidden"); // Toggle the 'hidden' class
+}
 //when recieved message back from worker
 worker.onmessage = (event) => {
   console.log(`Received message from worker: ${event.data}`);
   console.log("Main <===== Worker        Server", event.data);
   result.textContent = event.data;
+  
+  if (event.data === "offline") {
+    if(state === "online"){
+    toggleBackdrop();
+    state="offline";
+  }else{
+    state="online";
+  }
+  }
 };
 
 bgButton.addEventListener("click", (event) => {
--- worker.js
+++ worker.js
@@ -1,36 +1,3 @@
-// const work = () => {
-//   postMessage(intervalTime);
-//   intervalTime
-// }
-
-// onmessage = function (message) {
-//   // console.log("Worker: Message received from main script");
-//   console.log("Main =====> Worker        Server", message.data);
-
-//   const MAX_INTERVAL_TIME = 1000 * 60 * 5;
-//   setTimeout(
-//     // async () => {
-//     () => {
-//       postMessage(intervalTime);
-// try {
-//   const response = await fetch(url, options);
-//   const result = await response.text();
-//   const { status } = response;
-//   //if it were no connection and it was restored
-//   if (noConnection != 1) {
-//     noConnection = 1;
-//     postMessage("Connection restored");
-//   } //connection restored
-//   console.log(
-//     `message from main script to worker: ${message.data}, ${result}, status is: ${status},`
-//   );
-//   // console.log("Posting message back to main script");
-//   console.log("Main        Worker <===== Server", result);
-//   postMessage(`${result}`);
-// },
-//     intervalTime
-//   );
-// };
 const url = "http://localhost:3001/";
 const options = {
   method: "GET",
@@ -54,10 +21,11 @@ const work = async () => {
     const response = await fetch(url, options);
     const result = await response.text();
     const { status } = response;
+    postMessage("online");
     //if there were no connection and it was restored
     if (noConnection) {
       noConnection = 0;
-      postMessage("Connection restored");
+      console.log("Connection restored");
     }
     console.log("Posting message back to main script");
     console.log("Main        Worker <===== Server", result);
@@ -65,6 +33,7 @@ const work = async () => {
     postMessage(`${result}`);
   } catch (error) {
     console.error("Error", error);
+    postMessage("offline");
     noConnection += 1; //counts how many times there were no connection to server
     if (intervalTime < MAX_INTERVAL_TIME) {
       intervalTime += 1000;
